d178b030e75afbccb7ee5b87ded84338cdec93e1
Brian Hessel <brian.hessel@student.vives.be>
2024-11-03 12:47:19 +0100

    database verbinding + test van het weergeven van data

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import config.config as myconfig
+from config import config as myconfig
 import sysconfig
 from services.general_services import print_env_var
+from services.db_service import lees_db as mydbservice
 
+
+for row in mydbservice():
+	print(row)
